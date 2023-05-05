01b921a3097104a4838c6b162cb15e20f1f06212
Edouard1er <edouardchevenslove98@gmail.com>
2023-05-05 18:52:44 +0300

    Activate debug and auto actualisation des pages web

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template
+from flask_debugtoolbar import DebugToolbarExtension
 from controllers import files
 
 app = Flask(__name__)
+
+toolbar = DebugToolbarExtension(app)
 
 @app.route('/')
 def home():
     projectName = "Projet TCP-IP"
     fileList = files.getFiles()
     return render_template('home.html', projectName=projectName)
+
+if __name__ == '__main__':
+    app.run(debug=True)
